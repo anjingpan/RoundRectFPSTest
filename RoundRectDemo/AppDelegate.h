e6812447ce8f690ec3b7045ad499070475717b21
anjingpanI <172244892@qq.com>
2017-04-06 14:42:56 +0800

    Initial Commit

---
+++ RoundRectDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RoundRectDemo
+//
+//  Created by mac on 17/4/6.
+//  Copyright © 2017年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
